ef69240a1dd6b4ef56dba5230e6bb73fb4d929b8
pHaliak <p.haliak@icloud.com>
2026-07-02 22:30:32 +0200

    LASTONE STABEL

--- src/data/atlasStats.js
+++
@@ -1,8 +0,0 @@
-import { projects } from "./projects";
-
-export function getAtlasPhotoCount() {
-  return projects.reduce((total, project) => {
-    const images = project.studentImages || project.images || [];
-    return total + images.length;
-  }, 0);
-}
